472e513c2b89c648c5d63efef732488f2f8255ec
Tanner Stephens <tannerrstephens@gmail.com>
2025-11-09 11:37:24 -0600

    Owner can not see claimed items

--- wishlog/static/js/home.js
+++ wishlog/static/js/home.js
@@ -1,6 +1,6 @@
 const listEntryTemplate = Handlebars.compile(`
     <div class="column is-one-third-desktop">
-        <div class="card {{#if claimed}}is-claimed{{/if}}">
+        <div class="card {{#if not owner}}{{#if claimed}}is-claimed{{/if}}{{/if}}">
             <div class="card-image">
                 {{#if link}}<a href="{{ link }}" target="_blank">{{/if}}
                 <figure class="image is-4by3">
@@ -73,199 +73,199 @@ const listEntryTemplate = Handlebars.compile(`
             </div>
         </div>
     </div>
-`)
+`);
 
-Handlebars.registerHelper('price', function (cost) {
-  return `\$${cost.toFixed(2)}`
-})
+Handlebars.registerHelper("price", function (cost) {
+  return `\$${cost.toFixed(2)}`;
+});
 
 window.onload = () => {
-  const newFormParent = document.getElementById('new')
-  const list = document.getElementById('list')
+  const newFormParent = document.getElementById("new");
+  const list = document.getElementById("list");
 
-  const newItemForm = document.forms['newItemForm']
-  const submitButton = document.getElementById('submit')
-  const fileName = document.getElementById('fileName')
+  const newItemForm = document.forms["newItemForm"];
+  const submitButton = document.getElementById("submit");
+  const fileName = document.getElementById("fileName");
 
-  const claimedSwitch = document.getElementById('claimedSwitch')
+  const claimedSwitch = document.getElementById("claimedSwitch");
 
-  const temp = document.getElementById('temp')
+  const temp = document.getElementById("temp");
 
-  const orderBy = document.getElementById('orderBy')
+  const orderBy = document.getElementById("orderBy");
 
-  let owner = false
+  let owner = false;
 
   const appendItem = (item, replace) => {
-    const div = document.createElement('div')
-    div.innerHTML = listEntryTemplate({ ...item, owner }).trim()
+    const div = document.createElement("div");
+    div.innerHTML = listEntryTemplate({ ...item, owner }).trim();
 
-    const element = div.firstChild
+    const element = div.firstChild;
 
     if (replace instanceof HTMLDivElement) {
-      replace.replaceWith(element)
+      replace.replaceWith(element);
     } else {
-      list.prepend(element)
+      list.prepend(element);
     }
 
     if (owner) {
       document.getElementById(`delete-${item.id}`).onclick = () => {
         fetch(`/api/items/${item.id}`, {
-          method: 'DELETE'
+          method: "DELETE",
         })
-          .then(response => response.json())
-          .then(data => {
+          .then((response) => response.json())
+          .then((data) => {
             if (data.success) {
-              element.remove()
+              element.remove();
             }
-          })
-      }
+          });
+      };
 
-      const desireInput = document.getElementById(`desire-${item.id}`)
-      const costInput = document.getElementById(`cost-${item.id}`)
+      const desireInput = document.getElementById(`desire-${item.id}`);
+      const costInput = document.getElementById(`cost-${item.id}`);
 
       const patchItem = () => {
         fetch(`/api/items/${item.id}`, {
-          method: 'PATCH',
+          method: "PATCH",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             desire: desireInput.value,
-            cost: costInput.value
-          })
-        })
-      }
+            cost: costInput.value,
+          }),
+        });
+      };
 
-      desireInput.onchange = patchItem
-      costInput.onchange = patchItem
+      desireInput.onchange = patchItem;
+      costInput.onchange = patchItem;
     } else if (item.justclaimed) {
       document.getElementById(`unclaim-${item.id}`).onclick = () => {
         fetch(`/api/items/${item.id}/unclaim`, {
-          method: 'POST'
+          method: "POST",
         })
-          .then(response => response.json())
-          .then(data => {
+          .then((response) => response.json())
+          .then((data) => {
             if (data.success) {
-              item.claimed = false
-              item.justclaimed = false
+              item.claimed = false;
+              item.justclaimed = false;
 
-              appendItem(item, element)
+              appendItem(item, element);
             } else {
-              location.reload()
+              location.reload();
             }
-          })
-      }
+          });
+      };
     } else if (!item.claimed) {
       document.getElementById(`claim-${item.id}`).onclick = () => {
         fetch(`/api/items/${item.id}/claim`, {
-          method: 'POST'
+          method: "POST",
         })
-          .then(response => response.json())
-          .then(data => {
+          .then((response) => response.json())
+          .then((data) => {
             if (data.success) {
-              item.claimed = true
-              item.justclaimed = true
+              item.claimed = true;
+              item.justclaimed = true;
 
-              appendItem(item, element)
+              appendItem(item, element);
             } else {
-              location.reload()
+              location.reload();
             }
-          })
-      }
+          });
+      };
     }
-  }
+  };
 
   const loadAllItems = () => {
-    temp.innerHTML = list.innerHTML
-    list.innerHTML = ''
+    temp.innerHTML = list.innerHTML;
+    list.innerHTML = "";
 
-    const showClaimed = claimedSwitch.checked ? '&show_claimed' : ''
-    const order_by_value = orderBy.value.split('-')
-    const desc = order_by_value[1] == 'desc' ? '&desc' : ''
+    const showClaimed = claimedSwitch.checked ? "&show_claimed" : "";
+    const orderByValue = orderBy.value.split("-");
+    const desc = orderByValue[1] == "desc" ? "&desc" : "";
 
-    fetch(`/api/items?order_by=${order_by_value[0]}${desc}${showClaimed}`)
-      .then(response => response.json())
-      .then(data => {
-        data.items.reverse()
-        data.items.forEach(appendItem)
-        temp.innerHTML = ''
-      })
-  }
+    fetch(`/api/items?order_by=${orderByValue[0]}${desc}${showClaimed}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.items.reverse();
+        data.items.forEach(appendItem);
+        temp.innerHTML = "";
+      });
+  };
 
-  fetch('/api/session')
-    .then(response => response.json())
-    .then(data => {
+  fetch("/api/session")
+    .then((response) => response.json())
+    .then((data) => {
       if (data.user) {
-        newFormParent.classList.remove('is-hidden')
-        owner = true
+        newFormParent.classList.remove("is-hidden");
+        owner = true;
       }
 
-      loadAllItems(claimedSwitch.checked)
-    })
+      loadAllItems(claimedSwitch.checked);
+    });
 
-  const createItem = item => {
-    fetch('/api/items', {
-      method: 'POST',
+  const createItem = (item) => {
+    fetch("/api/items", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(item)
+      body: JSON.stringify(item),
     })
-      .then(response => response.json())
-      .then(data => appendItem(data.item))
-  }
+      .then((response) => response.json())
+      .then((data) => appendItem(data.item));
+  };
 
-  newItemForm.onsubmit = e => {
-    e.preventDefault()
+  newItemForm.onsubmit = (e) => {
+    e.preventDefault();
 
     if (submitButton.disabled) {
-      return
+      return;
     }
 
     const body = {
       title: newItemForm.title.value,
       desire: newItemForm.desire.value,
-      cost: newItemForm.cost.value
-    }
+      cost: newItemForm.cost.value,
+    };
 
     if (newItemForm.link.value) {
-      body.link = newItemForm.link.value
+      body.link = newItemForm.link.value;
     }
 
     if (newItemForm.image.value) {
-      const reader = new FileReader()
+      const reader = new FileReader();
 
       reader.onload = () => {
-        body.image = reader.result.split(',', 2)[1]
-        createItem(body)
-      }
+        body.image = reader.result.split(",", 2)[1];
+        createItem(body);
+      };
 
-      reader.readAsDataURL(newItemForm.image.files[0])
-      return
+      reader.readAsDataURL(newItemForm.image.files[0]);
+      return;
     }
 
-    createItem(body)
-  }
+    createItem(body);
+  };
 
   const setState = () => {
     submitButton.disabled =
       newItemForm.title.value.length == 0 ||
       newItemForm.cost.value.length == 0 ||
-      newItemForm.desire.value.length == 0
+      newItemForm.desire.value.length == 0;
 
     if (newItemForm.image.value) {
-      fileName.innerText = newItemForm.image.files[0].name
+      fileName.innerText = newItemForm.image.files[0].name;
     }
-  }
+  };
 
-  newItemForm.oninput = setState
-  setState()
+  newItemForm.oninput = setState;
+  setState();
 
   claimedSwitch.onchange = () => {
-    loadAllItems()
-  }
+    loadAllItems();
+  };
 
   orderBy.onchange = () => {
-    loadAllItems()
-  }
-}
+    loadAllItems();
+  };
+};
